95eb871dc8667a7a0fe8556f79d40ac6808d9aa6
chinakook <chinakook@msn.com>
2018-03-16 22:07:07 +0800

    Mantainable and readable enhancements

--- MobileNetV2.py
+++ MobileNetV2.py
@@ -23,71 +23,77 @@ def Conv1x1(channels, is_linear=False):
         out.add(nn.Activation('relu'))
     return out
 
-def DWise(channels, stride):
+def DWise(channels, strides):
     out = nn.HybridSequential()
     out.add(
-        nn.Conv2D(channels, 3, strides=stride, padding=1, groups=channels, use_bias=False),
+        nn.Conv2D(channels, 3, strides=strides, padding=1, groups=channels, use_bias=False),
         nn.BatchNorm(scale=True),
         nn.Activation('relu')
     )
     return out
 
 class InvertedResidual(nn.HybridBlock):
-    def __init__(self, t, e, c, s, same_shape=True, **kwargs):
+    def __init__(self, inp, oup, t, strides, same_shape=True, **kwargs):
         super(InvertedResidual, self).__init__(**kwargs)
         self.same_shape = same_shape
-        self.stride = s
+        self.strides = strides
         with self.name_scope(): 
             self.bottleneck = nn.HybridSequential()
             self.bottleneck.add(
-                Conv1x1(e*t),
-                DWise(e*t, self.stride),
-                Conv1x1(c, is_linear=True)
+                Conv1x1(inp*t),
+                DWise(inp*t, self.strides),
+                Conv1x1(oup, is_linear=True)
             )
-            if self.stride == 1 and not self.same_shape:
-                self.conv_res = Conv1x1(c)
+            #if self.stride == 1 and not self.same_shape:
+            #    self.conv_res = Conv1x1(oup)
     def hybrid_forward(self, F, x):
         out = self.bottleneck(x)
-        #if self.stride == 1 and self.same_shape:
-        #    out = F.elemwise_add(out, x)
-        if self.stride == 1:
-            if not self.same_shape:
-                x = self.conv_res(x)
+        if self.strides == 1 and self.same_shape:
             out = F.elemwise_add(out, x)
+        # if self.stride == 1:
+        #     if not self.same_shape:
+        #         x = self.conv_res(x)
+        #     out = F.elemwise_add(out, x)
         return out
+
+def InvertedResidualSequence(t, inp, oup, repeats, first_strides):
+    seq = nn.HybridSequential()
+    seq.add(InvertedResidual(inp, oup, t, first_strides, same_shape=False))
+    for _ in range(1, repeats):
+        seq.add(InvertedResidual(inp, oup, t, 1))
+    return seq, oup
 
 class MobilenetV2(nn.HybridBlock):
     def __init__(self, num_classes=1000, width_mult=1.0, **kwargs):
         super(MobilenetV2, self).__init__(**kwargs)
         
         self.w = width_mult
-        
-        self.cn = [int(x*self.w) for x in [32, 16, 24, 32, 64, 96, 160, 320]]
-        
-        def InvertedResidualSequence(t, cn_id, n, s):
-            seq = nn.HybridSequential()
-            seq.add(InvertedResidual(t, self.cn[cn_id-1], self.cn[cn_id], s, same_shape=False))
-            for _ in range(n-1):
-                seq.add(InvertedResidual(t, self.cn[cn_id-1], self.cn[cn_id], 1))
-            return seq
-        
-        self.b0 = ConvBlock(self.cn[0], 3, 2)
-        self.b1 = InvertedResidualSequence(1, 1, 1, 1)
-        self.b2 = InvertedResidualSequence(6, 2, 2, 2)
-        self.b3 = InvertedResidualSequence(6, 3, 3, 2)
-        self.b4 = InvertedResidualSequence(6, 4, 4, 2)
-        self.b5 = InvertedResidualSequence(6, 5, 3, 1)
-        self.b6 = InvertedResidualSequence(6, 6, 3, 2)
-        self.b7 = InvertedResidualSequence(6, 7, 1, 1)
 
+        self.first_oup = 32 * self.w
+        self.interverted_residual_setting = [
+            # t, c, n, s
+            [1, 16, 1, 1],
+            [6, 24, 2, 2],
+            [6, 32, 3, 2],
+            [6, 64, 4, 2],
+            [6, 96, 3, 1],
+            [6, 160, 3, 2],
+            [6, 320, 1, 1],           
+        ]
         self.last_channels = int(1280*self.w) if self.w > 1.0 else 1280
+
         with self.name_scope():
             self.features = nn.HybridSequential()
-            with self.features.name_scope():
-                self.features.add(self.b0, self.b1, self.b2, self.b3, self.b4, self.b5, self.b6, self.b7)
-                self.features.add(Conv1x1(self.last_channels))
-                self.features.add(nn.GlobalAvgPool2D())
-                self.features.add(nn.Flatten())
+            self.features.add(ConvBlock(self.first_oup, 3, 2))
+            inp = self.first_oup
+            for t, c, n, s in self.interverted_residual_setting:
+                irs, oup = InvertedResidualSequence(t, inp, c * self.w, n, s)
+                self.features.add(irs)
+                inp = oup
+
+            self.features.add(Conv1x1(self.last_channels))
+            self.features.add(nn.GlobalAvgPool2D())
+            self.features.add(nn.Flatten())
             self.output = nn.Dense(num_classes)
     def hybrid_forward(self, F, x):
         x = self.features(x)
