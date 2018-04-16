084fe0b851655517cf4d0535418871f7a64d03ce
chinakook <chinakook@msn.com>
2018-04-16 13:29:40 +0800

    RELU6 added; namescope updated; model updated; accuracy up

--- MobileNetV2.py
+++ MobileNetV2.py
@@ -4,65 +4,70 @@ import mxnet.gluon as gluon
 import mxnet.gluon.nn as nn
 import mxnet.autograd as ag
 
-def ConvBlock(channels, kernel_size, strides):
-    out = nn.HybridSequential()
-    out.add(
-        nn.Conv2D(channels, kernel_size, strides=strides, padding=1, use_bias=False),
-        nn.BatchNorm(scale=True),
-        nn.Activation('relu')
-    )
+class RELU6(nn.HybridBlock):
+    def __init__(self, **kwargs):
+        super(RELU6, self).__init__(**kwargs)
+
+    def hybrid_forward(self, F, x):
+        return F.clip(x, 0, 6, name="fwd")
+
+def ConvBlock(channels, kernel_size, strides, **kwargs):
+    out = nn.HybridSequential(**kwargs)
+    with out.name_scope():
+        out.add(
+            nn.Conv2D(channels, kernel_size, strides=strides, padding=1, use_bias=False),
+            nn.BatchNorm(scale=True),
+            RELU6(prefix="relu6_")
+        )
     return out
 
-def Conv1x1(channels, is_linear=False):
-    out = nn.HybridSequential()
-    out.add(
-        nn.Conv2D(channels, 1, padding=0, use_bias=False),
-        nn.BatchNorm(scale=True)
-    )
-    if not is_linear:
-        out.add(nn.Activation('relu'))
+def Conv1x1(channels, is_linear=False, **kwargs):
+    out = nn.HybridSequential(**kwargs)
+    with out.name_scope():
+        out.add(
+            nn.Conv2D(channels, 1, padding=0, use_bias=False),
+            nn.BatchNorm(scale=True)
+        )
+        if not is_linear:
+            out.add(RELU6(prefix="relu6_"))
     return out
 
-def DWise(channels, strides):
-    out = nn.HybridSequential()
-    out.add(
-        nn.Conv2D(channels, 3, strides=strides, padding=1, groups=channels, use_bias=False),
-        nn.BatchNorm(scale=True),
-        nn.Activation('relu')
-    )
+def DWise(channels, strides, **kwargs):
+    out = nn.HybridSequential(**kwargs)
+    with out.name_scope():
+        out.add(
+            nn.Conv2D(channels, 3, strides=strides, padding=1, groups=channels, use_bias=False),
+            nn.BatchNorm(scale=True),
+            RELU6(prefix="relu6_")
+        )
     return out
 
-class InvertedResidual(nn.HybridBlock):
+class ExpandedConv(nn.HybridBlock):
     def __init__(self, inp, oup, t, strides, same_shape=True, **kwargs):
-        super(InvertedResidual, self).__init__(**kwargs)
+        super(ExpandedConv, self).__init__(**kwargs)
         self.same_shape = same_shape
         self.strides = strides
         with self.name_scope(): 
             self.bottleneck = nn.HybridSequential()
             self.bottleneck.add(
-                Conv1x1(inp*t),
-                DWise(inp*t, self.strides),
-                Conv1x1(oup, is_linear=True)
+                Conv1x1(inp*t, prefix="expand_"),
+                DWise(inp*t, self.strides, prefix="dwise_"),
+                Conv1x1(oup, is_linear=True, prefix="linear_")
             )
-            #if self.stride == 1 and not self.same_shape:
-            #    self.conv_res = Conv1x1(oup)
     def hybrid_forward(self, F, x):
         out = self.bottleneck(x)
         if self.strides == 1 and self.same_shape:
             out = F.elemwise_add(out, x)
-        # if self.stride == 1:
-        #     if not self.same_shape:
-        #         x = self.conv_res(x)
-        #     out = F.elemwise_add(out, x)
         return out
 
-def InvertedResidualSequence(t, inp, oup, repeats, first_strides):
-    seq = nn.HybridSequential()
-    seq.add(InvertedResidual(inp, oup, t, first_strides, same_shape=False))
-    curr_inp = oup
-    for _ in range(1, repeats):
-        seq.add(InvertedResidual(curr_inp, oup, t, 1))
+def ExpandedConvSequence(t, inp, oup, repeats, first_strides, **kwargs):
+    seq = nn.HybridSequential(**kwargs)
+    with seq.name_scope():
+        seq.add(ExpandedConv(inp, oup, t, first_strides, same_shape=False))
         curr_inp = oup
+        for _ in range(1, repeats):
+            seq.add(ExpandedConv(curr_inp, oup, t, 1))
+            curr_inp = oup
     return seq
 
 class MobilenetV2(nn.HybridBlock):
@@ -74,26 +79,26 @@ class MobilenetV2(nn.HybridBlock):
         self.first_oup = 32 * self.w
         self.interverted_residual_setting = [
             # t, c, n, s
-            [1, 16, 1, 1],
-            [6, 24, 2, 2],
-            [6, 32, 3, 2],
-            [6, 64, 4, 2],
-            [6, 96, 3, 1],
-            [6, 160, 3, 2],
-            [6, 320, 1, 1],           
+            [1, 16, 1, 1, "stage0_"],  # -> 112x112
+            [6, 24, 2, 2, "stage1_"],  # -> 56x56
+            [6, 32, 3, 2, "stage2_"],  # -> 28x28
+            [6, 64, 4, 2, "stage3_0_"],  # -> 14x14
+            [6, 96, 3, 1, "stage3_1_"],  # -> 14x14
+            [6, 160, 3, 2, "stage4_0_"], # -> 7x7
+            [6, 320, 1, 1, "stage4_1_"], # -> 7x7          
         ]
         self.last_channels = int(1280*self.w) if self.w > 1.0 else 1280
 
         with self.name_scope():
             self.features = nn.HybridSequential()
-            self.features.add(ConvBlock(self.first_oup, 3, 2))
+            self.features.add(ConvBlock(self.first_oup, 3, 2, prefix="stage0_"))
             inp = self.first_oup
-            for t, c, n, s in self.interverted_residual_setting:
+            for t, c, n, s, prefix in self.interverted_residual_setting:
                 oup = c * self.w
-                self.features.add(InvertedResidualSequence(t, inp, oup, n, s))
+                self.features.add(ExpandedConvSequence(t, inp, oup, n, s, prefix=prefix))
                 inp = oup
 
-            self.features.add(Conv1x1(self.last_channels))
+            self.features.add(Conv1x1(self.last_channels, prefix="stage4_2_"))
             self.features.add(nn.GlobalAvgPool2D())
             self.features.add(nn.Flatten())
             self.output = nn.Dense(num_classes)
@@ -108,8 +113,10 @@ net = MobilenetV2(1000,1, prefix="")
 data =mx.sym.var('data')
 sym = net(data)
 
-# plot network graph
+# print network summary
 #mx.viz.print_summary(sym, shape={'data':(8,3,224,224)})
+
+# plot network graph
 #mx.viz.plot_network(sym,shape={'data':(8,3,224,224)}, node_attrs={'shape':'oval','fixedsize':'fasl==false'}).view()
 
 
--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 This is a Gluon implementation of MobileNetV2 architecture as described in the paper [Inverted Residuals and Linear Bottlenecks: Mobile Networks for Classification, Detection and Segmentation](https://arxiv.org/pdf/1801.04381).
 
-
 ### Pretrained Models on ImageNet
 
 We provide pretrained MobileNet models on ImageNet, which achieve slightly better accuracy rates than the original ones reported in the paper. 
@@ -11,8 +10,11 @@ The top-1/5 accuracy rates by using single center crop (crop size: 224x224, imag
 
 Network|Top-1|Top-5|
 :---:|:---:|:---:|
-MobileNet v2| 71.90| 90.49
+MobileNet v2| 72.45| 90.78
+
+### Tranning Script
+For tranning, please refer to [this repo](https://github.com/liangfu/mxnet-mobilenet-v2).Our pretrained model is converted from that.
 
 ### Normalization
 
-The input images are substrated by mean RGB = [ 123.68, 116.78, 103.94 ], and then multiplied by 0.017.
+The input images are substrated by mean RGB = [ 123.68, 116.78, 103.94 ].
